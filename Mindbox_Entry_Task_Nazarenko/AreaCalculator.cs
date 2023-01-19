9f960dd4543f5c2f7ef4919c53f8fff56d2260be
nsv-dev <nsvkart@bk.ru>
2023-01-19 19:29:00 +0300

    Добавьте файлы проекта.

---
+++ Mindbox_Entry_Task_Nazarenko/AreaCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Mindbox_Entry_Task_Nazarenko
+{
+    public class AreaCalculator
+    {
+        public static double CalculateAreaBySides(params double[] sides)
+        {
+
+        }
+    }
+}
---
+++ Mindbox_Entry_Task_NazarenkoTests/AreaCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mindbox_Entry_Task_Nazarenko;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mindbox_Entry_Task_Nazarenko.Tests
+{
+    [TestClass()]
+    public class AreaCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateAreaBySidesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
