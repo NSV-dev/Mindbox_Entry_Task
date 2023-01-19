776dbd227bcbed732aa1e47d20d8c23598270e5d
nsv-dev <nsvkart@bk.ru>
2023-01-19 19:30:30 +0300

    Added area calculation for 2 sides

--- Mindbox_Entry_Task_Nazarenko/AreaCalculator.cs
+++ Mindbox_Entry_Task_Nazarenko/AreaCalculator.cs
@@ -8,9 +8,16 @@
 
             if (sidesQuantity == 1)
                 return OneSide(sides[0]);
+
+            if (sidesQuantity == 2)
+                return TwoSides(sides[0], sides[1]);
+
         }
 
         private static double OneSide(double r) => Math.PI * r * r;
+
+        private static double TwoSides(double a, double b) => a * b;
+
 
     }
 }
